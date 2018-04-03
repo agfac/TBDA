951379cda7ca10e7627e495fcdeb494514f8dea6
André Correia <andregfcorreia@gmail.com>
2018-04-03 01:03:40 +0100

    Change exercise 4 queries - change where condition to join

--- Lab_1/queries/4y.sql
+++ Lab_1/queries/4y.sql
@@ -13,5 +13,4 @@ FROM horas_docentes
 GROUP BY tipo;
 
 SELECT nr, nome, max_horas_docentes.tipo, max_nr_horas
-FROM horas_docentes, max_horas_docentes
-WHERE horas_docentes.tipo = max_horas_docentes.tipo AND horas_docentes.nr_horas = max_horas_docentes.max_nr_horas;
+FROM horas_docentes join max_horas_docentes on horas_docentes.tipo = max_horas_docentes.tipo AND horas_docentes.nr_horas = max_horas_docentes.max_nr_horas;
