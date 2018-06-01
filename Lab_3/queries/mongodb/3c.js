8bb597604435d0851498ce61ceae7220c5e4a5bf
André Correia <andregfcorreia@gmail.com>
2018-06-02 00:18:18 +0100

    Add mongo query result files

--- Lab_3/queries/mongodb/3c.js
+++ Lab_3/queries/mongodb/3c.js
@@ -6,6 +6,9 @@ db.facilities.aggregate([
      $match: {"MUNICIPALITIES.ACTIVITIES.ACTIVITY": { $ne: 'cinema' }}
 },
 {
-    $group: {count: { $sum: 1 },_id:0}
+    $group: {count: { $sum: 1 },_id: 0}
+},
+{
+	$project: {_id: 0}
 }
 ])
--- Lab_3/queries/mongodb/3e.js
+++ Lab_3/queries/mongodb/3e.js
@@ -3,5 +3,5 @@ db.facilities.find(
   $nor: [
   		{"MUNICIPALITIES.ACTIVITIES": {$size: 0}}
   		]},
-  		{COD:1, DESIGNATION:1}
+  		{_id: 0, COD:1, DESIGNATION:1}
 )
