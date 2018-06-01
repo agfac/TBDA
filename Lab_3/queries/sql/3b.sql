0a09dc4637784ce2051a7bb463f74665253060a4
André Correia <andregfcorreia@gmail.com>
2018-06-01 17:00:01 +0100

    Refactor 3b SQL query

--- Lab_3/queries/sql/3b.sql
+++ Lab_3/queries/sql/3b.sql
@@ -1,4 +1,4 @@
-SELECT reg.designation, COUNT(*)
+SELECT reg.designation, COUNT(*) as num_facilities
 FROM facilities f
 JOIN roomtypes r
 ON f.roomtype = r.roomtype
@@ -7,4 +7,5 @@ ON f.municipality = m.cod
 JOIN regions reg
 ON m.region = reg.cod
 WHERE r.description LIKE '%touros%'
-GROUP BY reg.designation;
+GROUP BY reg.designation
+ORDER BY reg.designation;
